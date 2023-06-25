b072b1c364f127edb0d1fdd2c09555886a06ba46
Soryn Bächli <soryn.baechli@gmail.com>
2023-06-25 23:51:26 +0200

    feat: Added custom colors and tailwind-scrollbar
    the tailwind-scrollbar plugin is added to style scrollbars inline instead of styling them inside a base.css file.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config}*/
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+    content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {},
-  },
+    theme: {
+        colors: {
+            ink: {
+                400: '#16141D',
+                500: '#1D1A23',
+                600: '#2F2D34',
+            },
+            nebula: {
+                400: '#542DC1',
+                500: '#6F00FF',
+                600: '#8666FB',
+            },
+            neon: {
+                500: '#FF63F7',
+            },
+            snow: {
+                500: '#F0FBFF',
+            },
+        },
+        extend: {},
+    },
 
-  plugins: [],
+    plugins: [require('tailwind-scrollbar')({ nocompatible: true })],
 };
 
 module.exports = config;
